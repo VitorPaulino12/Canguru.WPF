d27c9832006cdd5447360e2c3d9025893d8f43fa
Vitor Paulino <vitoraugustoj2005@gmail.com>
2025-11-17 14:46:40 -0300

    Criação de telas de perfil

---
+++ Canguru.WPF/EditarPerfilWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Canguru.WPF
+{
+    /// <summary>
+    /// Lógica interna para EditarPerfilWindow.xaml
+    /// </summary>
+    public partial class EditarPerfilWindow : Window
+    {
+        public EditarPerfilWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelarAtualiza_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Canguru.WPF/PerfilWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Canguru.WPF
+{
+    /// <summary>
+    /// Lógica interna para PerfilWindow.xaml
+    /// </summary>
+    public partial class PerfilWindow : Window
+    {
+        public PerfilWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEditarInformacoes_Click(object sender, RoutedEventArgs e)
+        {
+            EditarPerfilWindow Editar = new EditarPerfilWindow();
+            Editar.Show();
+        }
+
+        private void btnEditarSobreMim_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
